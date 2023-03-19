8dce1cb4cc67e46017c5dbbf0159035db153d951
Eugene Syrotiuk <eugenesyuk@gmail.com>
2023-03-19 16:52:17 +0100

    Add board labels, Lato font

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,24 +1,76 @@
-
-import { Board } from '../core/Board'
-import { FC } from 'react'
-import React from "react"
-import { CellComponent } from '.'
+import { Board } from "../core/Board";
+import { FC } from "react";
+import React from "react";
+import { CellComponent } from ".";
 
 interface BoardProps {
-  board: Board,
-  setBoard: (board: Board) => void
+  board: Board;
+  setBoard: (board: Board) => void;
 }
 
 export const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
   return (
-    <div className="board">
-      <>
-        {board.cells.map((row, index) => 
-          <React.Fragment>
-            {row.map(cell => <CellComponent cell={cell} key={cell.id} />)}
-          </React.Fragment>
-        )}
-      </>
+    <div className="frame">
+      <BoardLabels/>
+      <div className="board">
+        <>
+          {board.cells.map((row, index) => (
+            <React.Fragment>
+              {row.map((cell) => (
+                <CellComponent cell={cell} key={cell.id} />
+              ))}
+            </React.Fragment>
+          ))}
+        </>
+      </div>
     </div>
-  )
+  );
 }
+
+const BoardLabels = () => (
+  <>
+    <div className="labels top">
+      <span>A</span>
+      <span>B</span>
+      <span>C</span>
+      <span>D</span>
+      <span>E</span>
+      <span>F</span>
+      <span>G</span>
+      <span>H</span>
+    </div>
+
+    <div className="labels right">
+      <span>8</span>
+      <span>7</span>
+      <span>6</span>
+      <span>5</span>
+      <span>4</span>
+      <span>3</span>
+      <span>2</span>
+      <span>1</span>
+    </div>
+
+    <div className="labels bottom">
+      <span>A</span>
+      <span>B</span>
+      <span>C</span>
+      <span>D</span>
+      <span>E</span>
+      <span>F</span>
+      <span>G</span>
+      <span>H</span>
+    </div>
+
+    <div className="labels left">
+      <span>8</span>
+      <span>7</span>
+      <span>6</span>
+      <span>5</span>
+      <span>4</span>
+      <span>3</span>
+      <span>2</span>
+      <span>1</span>
+    </div>
+  </>
+)
