0c0a71b905edc88adca625f7003b9b2bc9059d5f
Eugene Syrotiuk <eugenesyuk@gmail.com>
2023-03-20 23:53:08 +0100

    Add bishop movement validation logic

--- src/core/figures/Bishop.ts
+++ src/core/figures/Bishop.ts
@@ -9,7 +9,8 @@ export class Bishop extends Figure {
 
   canMove(target: Cell) {
     let allow = true
-    allow = super.canMove(target) ? allow : false    
+    allow = super.canMove(target) ? allow : false
+    allow = this.cell.isDiagonalPathFree(target) ? allow : false
     return allow
   }
 }
