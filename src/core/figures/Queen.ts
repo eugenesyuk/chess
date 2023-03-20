1107d22a79f179da44926da726bc39830770c098
Eugene Syrotiuk <eugenesyuk@gmail.com>
2023-03-20 23:48:44 +0100

    Added Queen canMove method logic

--- src/core/figures/Queen.ts
+++ src/core/figures/Queen.ts
@@ -1,6 +1,6 @@
-import { Color, FigureName } from '../Enums';
-import { Figure } from '../Figure';
-import { Cell } from '../Cell';
+import { Color, FigureName } from '../Enums'
+import { Figure } from '../Figure'
+import { Cell } from '../Cell'
 
 export class Queen extends Figure {
   constructor(color: Color, cell: Cell) {
@@ -8,8 +8,14 @@ export class Queen extends Figure {
   }
 
   canMove(target: Cell) {
-    let allow = true
-    allow = super.canMove(target) ? allow : false    
+    let allow = true;
+    allow = super.canMove(target) ? allow : false;
+    allow =
+      this.cell.isHorizonatalPathFree(target) ||
+      this.cell.isVerticalPathFree(target) ||
+      this.cell.isDiagonalPathFree(target)
+        ? allow
+        : false
     return allow
   }
 }
